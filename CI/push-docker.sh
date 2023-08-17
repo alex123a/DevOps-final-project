a84373e71e022f619bb4090f328c1ec71324867d
Alex <alval20@student.sdu.dk>
2023-08-17 09:06:24 +0000

    Adjusted build-docker and push-docker to this project #6

--- CI/push-docker.sh
+++ CI/push-docker.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 echo "$docker_password" | docker login ghcr.io --username "$docker_username" --password-stdin
-docker push "ghcr.io/$docker_username/micronaut-app:1.0-${GIT_COMMIT::8}" 
-docker push "ghcr.io/$docker_username/micronaut-app:latest" &
+docker push "ghcr.io/$docker_username/fortune-cookie-backend:1.0-${GIT_COMMIT::8}" 
+docker push "ghcr.io/$docker_username/fortune-cookie-backend:latest" &
+docker push "ghcr.io/$docker_username/fortune-cookie-frontend:1.0-${GIT_COMMIT::8}" 
+docker push "ghcr.io/$docker_username/fortune-cookie-frontend:latest" &
 wait
